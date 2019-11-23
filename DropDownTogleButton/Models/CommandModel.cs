b1e689db4e11b9099590c6e8c4a560cb18e5e704
samuel carreau <samuelcarreau@gmail.com>
2019-11-23 15:13:23 -0500

    *ignore is working

---
+++ DropDownTogleButton/Models/CommandModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DropDownTogleButton.Models
+{
+    public class CommandModel
+    {
+        public string Name { get; set; }
+        public ICommand Command { get; set; }
+        public object CommandParameter { get; set; }
+
+        public CommandModel(string name, ICommand command, object commandParameter = null)
+        {
+            Name = name;
+            Command = command;
+            CommandParameter = commandParameter;
+        }
+    }
+}
