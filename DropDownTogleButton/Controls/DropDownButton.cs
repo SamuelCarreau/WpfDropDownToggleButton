99236c9c05b47dd4c6ff78142926283f04f4890b
samuel carreau <samuelcarreau@gmail.com>
2019-11-23 14:12:40 -0500

    initialcommit

---
+++ DropDownTogleButton/Controls/DropDownButton.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+
+namespace DropDownTogleButton.Controls
+{
+    // https://www.codeproject.com/Tips/1136681/WPF-Drop-Down-Menu-Button
+    public class DropDownButton : ToggleButton
+    {
+        public ContextMenu Menu
+        {
+            get { return (ContextMenu)GetValue(MenuProperty); }
+            set { SetValue(MenuProperty, value); }
+        }
+
+        public ItemsControl Items
+        {
+            get { return (ItemsControl)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+
+        public static readonly DependencyProperty MenuProperty = DependencyProperty.Register(nameof(Menu),
+            typeof(ContextMenu), typeof(DropDownButton), new UIPropertyMetadata(null, OnMenuChanged));
+
+        public static readonly DependencyProperty ItemsProperty = DependencyProperty.Register(nameof(Items),
+            typeof(ItemsControl), typeof(DropDownButton), new UIPropertyMetadata(null,OnItemsChanged));
+
+        private static void OnItemsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DropDownButton()
+        {
+            // Bind the ToogleButton.IsChecked property to the drop-down's IsOpen property
+            Binding binding = new Binding("Menu.IsOpen");
+            binding.Source = this;
+            this.SetBinding(IsCheckedProperty, binding);
+            DataContextChanged += (sender, args) =>
+            {
+                if (Menu != null)
+                    Menu.DataContext = DataContext;
+            };
+        }
+
+        private static void OnMenuChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var dropDownButton = (DropDownButton)d;
+            var contextMenu = (ContextMenu)e.NewValue;
+            contextMenu.DataContext = dropDownButton.DataContext;
+        }
+
+        protected override void OnClick()
+        {
+            if (Menu != null)
+            {
+                Menu.PlacementTarget = this;
+                Menu.Placement = PlacementMode.Right;
+                Menu.IsOpen = true;
+            }
+        }
+    }
+}
---
+++ DropDownTogleButton/ViewModels/ButtonViewModel.cs
@@ -0,0 +1,50 @@
+﻿using DropDownTogleButton.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+using WpfTestingApp.Infrastucture.DelegateCommand;
+
+namespace DropDownTogleButton.ViewModels
+{
+    public class ButtonViewModel : ViewModelBase
+    {
+        public List<CommandModel> Commands { get; private set; }
+        private Brush _color;
+        public Brush Color
+        {
+            get => _color;
+            set
+            {
+                _color = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ButtonViewModel()
+        {
+            Commands = new List<CommandModel>
+            {
+                new CommandModel("Yellow", new DelegateCommand(SetColorYellow)),
+                new CommandModel("Green", new DelegateCommand(SetColorGreen)),
+                new CommandModel("Red", new DelegateCommand(SetColorRed))
+            };
+        }
+
+        private void SetColorYellow(object obj)
+        {
+            Color = new SolidColorBrush(Colors.Yellow);
+        }
+        private void SetColorGreen(object obj)
+        {
+            Color = new SolidColorBrush(Colors.Green);
+        }
+        private void SetColorRed(object obj)
+        {
+            Color = new SolidColorBrush(Colors.Red);
+        }
+    }
+}
